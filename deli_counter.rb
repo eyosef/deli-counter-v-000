899eecd7dace825c0563a670340ffb31f4209394
Eden Mesfun <emesfun@gmail.com>
2018-01-27 23:33:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 def now_serving(katz_deli)
     puts "There is nobody waiting to be served!" if katz_deli.length == 0
 
-    katz_deli.each do |name = "Logan"|
+    katz_deli.each do |name|
       puts "Currently serving #{name}." if katz_deli.length >= 1
 
     end #each
